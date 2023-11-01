9997b01b585b264d7bfd2c500f210f97d56909d7
Mohanendra Siddha <msiddha2@unl.edu@MacBook-Air.lan>
2023-11-01 16:36:46 -0500

    loading and sending our contract(object) to console of our browser

--- src/app.js
+++ src/app.js
@@ -1,35 +1,46 @@
 App = {
-    loading: false,
-    contracts: {},
+    // loading: false,
+    contracts: {}, 
+
   
     load: async () => {
       await App.loadWeb3()
       await App.loadAccount()
+      await App.loadContract()
     },
   
     // https://medium.com/metamask/https-medium-com-metamask-breaking-change-injecting-web3-7722797916a8
     loadWeb3: async () => {
-        if (window.ethereum) {
-            window.web3 = new Web3(ethereum);
-            try {
-                // Request account access if needed
-                await ethereum.enable();
-                // Acccounts now exposed
-                web3.eth.sendTransaction({/* ... */});
-            } catch (error) {
-                // User denied account access...
-            }
+      if (typeof web3 !== 'undefined') {
+        App.web3Provider = web3.currentProvider
+        web3 = new Web3(web3.currentProvider)
+      } else {
+        App.web3Provider = new web3.providers.HttpProvider('http://127.0.0.1:7545');
+        window.alert("Please connect to Metamask.")
+      }
+      // Modern dapp browsers...
+      if (window.ethereum) {
+        window.web3 = new Web3(ethereum)
+        try {
+          // Request account access if needed
+          await ethereum.enable()
+          // Acccounts now exposed
+          web3.eth.sendTransaction({/* ... */})
+        } catch (error) {
+          // User denied account access...
         }
-        // Legacy dapp browsers...
-        else if (window.web3) {
-            window.web3 = new Web3(web3.currentProvider);
-            // Acccounts always exposed
-            web3.eth.sendTransaction({/* ... */});
-        }
-        // Non-dapp browsers...
-        else {
-            console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
-        }
+      }
+      // Legacy dapp browsers...
+      else if (window.web3) {
+        App.web3Provider = web3.currentProvider
+        window.web3 = new Web3(web3.currentProvider)
+        // Acccounts always exposed
+        web3.eth.sendTransaction({/* ... */})
+      }
+      // Non-dapp browsers...
+      else {
+        console.log('Non-Ethereum browser detected. You should consider trying MetaMask!')
+      }
     },
   
     loadAccount: async () => {
@@ -38,6 +49,14 @@ App = {
       console.log(web3.eth.defaultAccount)
     //   App.account = web3.eth.accounts[0]
     },
+
+    loadContract: async()=>{
+      const todoList=await $.getJSON('TodoList.json')
+      App.contracts.TodoList=TruffleContract(todoList)
+      App.contracts.TodoList.setProvider(App.web3Provider)
+      App.todoList = await App.contracts.TodoList.deployed()
+      console.log(todoList)
+    },   
   }
   
   $(() => {
