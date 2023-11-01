2f14cce75d279205678aa08a4985fac9d065d847
Mohanendra Siddha <msiddha2@unl.edu@MacBook-Air.lan>
2023-11-01 09:24:37 -0500

    working fine and retreiving the contracts address to console

--- src/app.js
+++ src/app.js
@@ -1,11 +1,47 @@
-App={
-    load:async()=>{
-        console.log("app loading")
-    }
-}
-
-$(()=>{
-    $(window).load(()=>{
-        App.load()
+App = {
+    loading: false,
+    contracts: {},
+  
+    load: async () => {
+      await App.loadWeb3()
+      await App.loadAccount()
+    },
+  
+    // https://medium.com/metamask/https-medium-com-metamask-breaking-change-injecting-web3-7722797916a8
+    loadWeb3: async () => {
+        if (window.ethereum) {
+            window.web3 = new Web3(ethereum);
+            try {
+                // Request account access if needed
+                await ethereum.enable();
+                // Acccounts now exposed
+                web3.eth.sendTransaction({/* ... */});
+            } catch (error) {
+                // User denied account access...
+            }
+        }
+        // Legacy dapp browsers...
+        else if (window.web3) {
+            window.web3 = new Web3(web3.currentProvider);
+            // Acccounts always exposed
+            web3.eth.sendTransaction({/* ... */});
+        }
+        // Non-dapp browsers...
+        else {
+            console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
+        }
+    },
+  
+    loadAccount: async () => {
+      // Set the current blockchain account
+      web3.eth.defaultAccount=web3.eth.accounts[0];
+      console.log(web3.eth.defaultAccount)
+    //   App.account = web3.eth.accounts[0]
+    },
+  }
+  
+  $(() => {
+    $(window).load(() => {
+      App.load()
     })
-})
+  })
